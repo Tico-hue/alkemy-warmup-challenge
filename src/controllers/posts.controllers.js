c093ac7b5ac3d988fbe1d2dead7ae94ea4cb86f6
Tico-hue <lautarotico@gmail.com>
2021-03-25 14:28:55 -0300

    feat: create post function added

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -26,3 +26,18 @@ const getPost = async (req = Request, res = Response) => {
   }
 };
 
+const createPost = async (req = Request, res = Response) => {
+  if (req.body.image.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)) {
+    const post = await sequelize.Post.create({
+      raw: true,
+      title: req.body.title,
+      content: req.body.content,
+      image: req.body.image,
+      cat_id: req.body.category,
+    });
+    res.status(201).json(post);
+  } else {
+    res.status(500).send("image field must be a image url format");
+  }
+};
+
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const {
   getAllposts,
+  createPost,
   getPost,
 } = require("../controllers/posts.controllers");
 const router = Router();
+
 router.get("/", getAllposts);
 router.get("/:id", getPost);
+router.post("/", createPost);
 module.exports = router;
