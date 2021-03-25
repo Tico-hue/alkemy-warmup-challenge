da8379a0ef24410041cac0a7cb2800169a4e3c09
Tico-hue <lautarotico@gmail.com>
2021-03-25 14:23:57 -0300

    refact: console.logs changed and swagger.json added

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,9 +1,10 @@
 const ExpressServer = require("./server/expressServer");
 const config = require("../config");
-
+const logger = require("../loaders/logger");
 module.exports = async () => {
   const server = new ExpressServer();
-  console.log("Express loaded");
+  logger.info("Express loaded");
   server.start();
   console.log(`Server listening on port ${config.port}`);
+  logger.info(`Server listening on port ${config.port}`);
 };
